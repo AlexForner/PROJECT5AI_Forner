1bd15ff99e19e318b215cd6d0776324fede793ca
Alex <alexforner9@gmail.com>
2018-11-15 09:32:33 +0100

    Inizialize project

---
+++ PROJECT5AI_FornerApp/PROJECT5AI_app/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PROJECT5AI_app
+//
+//  Created by Alex on 15/11/2018.
+//  Copyright © 2018 Class_4AI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+    
+    
+}
+
